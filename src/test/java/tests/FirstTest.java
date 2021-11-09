caee843b87e99fdde0f6e4638b86aa72affdc1b7
Daniel <burzynskidaniel90@gmail.com>
2021-11-09 18:47:54 +0100

    Initial commit

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+
+    @Test
+    public void firstTestJenkins(){
+        System.out.println("This is first test");
+    }
+}
