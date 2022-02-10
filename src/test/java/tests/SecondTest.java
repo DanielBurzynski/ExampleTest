3eedc318b05334af2c3c598f7603d19485109c51
Daniel <burzynskidaniel90@gmail.com>
2022-02-10 21:32:23 +0100

    Test5

--- src/test/java/tests/SecondTest.java
+++ src/test/java/tests/SecondTest.java
@@ -3,12 +3,12 @@ package tests;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class FirstTest {
+public class SecondTest {
 
     @Parameters({"Browser","testngpath"})
     @Test
     public void firstTestJenkins(String browser, String testngpath){
-        System.out.println("This is first test");
+        System.out.println("This is SECOND test");
         System.out.println(browser);
         System.out.println(testngpath);
     }
